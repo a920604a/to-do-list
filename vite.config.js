b89e182140f0d0ea41d5d3d75949b45340b06872
a920604a <a920604a@gmail.com>
2025-05-23 16:29:54 +0800

    fix: update base path in vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'to-do-list'
 export default defineConfig({
-  base: "to-do-list",
+  base: `/${repoName}/`, // GitHub Pages 路徑
   plugins: [react()]
 })
